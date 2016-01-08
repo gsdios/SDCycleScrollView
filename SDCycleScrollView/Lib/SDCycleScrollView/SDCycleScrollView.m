40b0ac70787001aec2972e37bd574bb48606bcdb
GSD_iOS <gsdios@126.com>
2016-01-08 16:17:03 +0800

    修改默认图片content mode

--- SDCycleScrollView/Lib/SDCycleScrollView/SDCycleScrollView.m
+++ SDCycleScrollView/Lib/SDCycleScrollView/SDCycleScrollView.m
@@ -89,7 +89,7 @@ NSString * const ID = @"cycleCell";
     _hidesForSinglePage = YES;
     _currentPageDotColor = [UIColor whiteColor];
     _pageDotColor = [UIColor lightGrayColor];
-    _bannerImageViewContentMode = UIViewContentModeScaleAspectFill;
+    _bannerImageViewContentMode = UIViewContentModeScaleToFill;
     
     self.backgroundColor = [UIColor lightGrayColor];
     
