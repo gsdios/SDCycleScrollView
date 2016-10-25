f4696dad051bad2786a865eed74b208e394d3437
Daboge <Dabo_iOS@163.com>
2016-10-25 16:44:01 +0800

    修改提示

--- SDCycleScrollView/Lib/SDCycleScrollView/SDCycleScrollView.m
+++ SDCycleScrollView/Lib/SDCycleScrollView/SDCycleScrollView.m
@@ -10,24 +10,18 @@
  
  *********************************************************************************
  *
- * 🌟🌟🌟 新建SDCycleScrollView交流QQ群：185534916 🌟🌟🌟
+ * 🌟🌟🌟 SDCycleScrollView修改版 🌟🌟🌟
  *
- * 在您使用此自动轮播库的过程中如果出现bug请及时以以下任意一种方式联系我们，我们会及时修复bug并
- * 帮您解决问题。
- * 新浪微博:GSD_iOS
- * Email : gsdios@126.com
- * GitHub: https://github.com/gsdios
+ * 根据SDCycleScrollView修改的，修改了一些存在的bug，并添加了个新功能，数据源可以是个UIView的数组。
+ * QQ: 382493496
+ * Email: Dabo_iOS@163.com
+ * GitHub: https://github.com/lianxingbo
+ * 
+ * 原版GitHub: https://github.com/gsdios/SDCycleScrollView
  *
- * 另（我的自动布局库SDAutoLayout）：
- *  一行代码搞定自动布局！支持Cell和Tableview高度自适应，Label和ScrollView内容自适应，致力于
- *  做最简单易用的AutoLayout库。
- * 视频教程：http://www.letv.com/ptv/vplay/24038772.html
- * 用法示例：https://github.com/gsdios/SDAutoLayout/blob/master/README.md
- * GitHub：https://github.com/gsdios/SDAutoLayout
  *********************************************************************************
  
  */
-
 
 #import "SDCycleScrollView.h"
 #import "SDCollectionViewCell.h"
