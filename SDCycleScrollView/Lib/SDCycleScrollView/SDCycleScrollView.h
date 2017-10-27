dddecc48cb2456d87eb8feeb367f4781c58d1e4c
clOudbb <z61723117@163.com>
2017-10-27 11:08:52 +0800

    Update：更新自定义cell说明

--- SDCycleScrollView/Lib/SDCycleScrollView/SDCycleScrollView.h
+++ SDCycleScrollView/Lib/SDCycleScrollView/SDCycleScrollView.h
@@ -67,7 +67,11 @@ typedef enum {
 
 // ========== 轮播自定义cell ==========
 
-/** 如果你需要自定义cell样式，请在实现此代理方法返回你的自定义cell的class。 */
+/**
+ * 如果你需要自定义cell样式，请在实现此代理方法
+ * 返回自定义cell的class。
+ * 如使用Xib创建cell，返回UINib
+ */
 - (id)customCollectionViewCellClassForCycleScrollView:(SDCycleScrollView *)view;
 
 /** 如果你自定义了cell样式，请在实现此代理方法为你的cell填充数据以及其它一系列设置 */
